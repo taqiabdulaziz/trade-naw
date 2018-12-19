f5ee2ba6905e075bdfe9053a630aa02be0c4c379
taqiabdulaziz <svx.annexiv@gmail.com>
2018-12-19 09:09:27 +0700

    change routing

--- app.js
+++ app.js
@@ -9,15 +9,37 @@ app.set('view engine', 'ejs');
 
 server.listen(3000)
 
-app.get('/', function (req, res) {
+app.get('/chatbox', function (req, res) {
     res.render(`chatbox.ejs`)
-
 });
 
-io.emit('some event', { for: 'everyone' });
+// io.to(123).emit(`event`, `aiwdjaw`)
+
+// io.on('chat message', function (data) {
+//     console.log(data);
+//     data.to(123).emit('chat message', data.msg);
+// });
+
+
+// socket.emit('subscribe', conversation_id);
+
+// socket.emit('send message', {
+//     room: conversation_id,
+//     message: "Some message"
+// });
+
+// socket.on('conversation private post', function(data) {
+//     //display data.message
+// });
+
 io.on('connection', function (socket) {
+    console.log(`user connected`);
+    
     socket.on('chat message', function (msg) {
-        io.emit('chat message', msg);
+        io.emit('chat message', `Taqi: ${msg}`);
+        console.log(msg);
+        
     });
 });
+
 
