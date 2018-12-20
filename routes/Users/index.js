3eb38c03987a27a2220e40688a880ad7c9f8c527
ACER <fauziaazia171097@gmail.com>
2018-12-20 06:20:41 -0500

    send email

--- routes/Users/index.js
+++ routes/Users/index.js
@@ -69,7 +69,6 @@ Routes.post("/buy/:currencyId", (req, res) => {
 })
 
 Routes.get('/request',checkLogin,checkrole, (req, res) => {
-
    Request.findAll({
        include:[{
            model:Currency
@@ -86,10 +85,86 @@ Routes.get('/request',checkLogin,checkrole, (req, res) => {
 })
 
 Routes.get('/request/:requestId', (req, res) => {
+    let insert = null
     Request.findOne({where:{id:req.params.requestId}})
         .then((request) => {
-            res.send(request)
+            request.status = "approved"
+            insert = {
+                UserId: request.UserId,
+                CurrencyId: request.CurrencyId,
+                amount: request.amount,
+                qtyPrice: request.qtyPrice,
+                subTotal: request.amount * request.qtyPrice
+            }
+            return Request.update({
+                UserId: request.UserId,
+                CurrencyId: request.CurrencyId,
+                amount: request.amount,
+                qtyPrice: request.qtyPrice,
+                status: "approved"
+            }, {where:{id:req.params.requestId}})
+            // res.send(request)
         })
+        .then((data) => {
+        //    console.log(insert)
+          return  TransactionB2B.create(insert)
+        })
+        .then(transaction => {
+           return User.findOne({
+                where: {id: transaction.UserId},
+                include:[{model:TransactionB2B}]
+            })
+        })
+        .then((user) => {
+                // res.send(user)
+                var nodemailer = require('nodemailer');
+
+            var transporter = nodemailer.createTransport({
+              service: 'gmail',
+              auth: {
+                user: 'gamecowo12345@gmail.com',
+                pass: 'gamecowo54321'
+              }
+            });
+            
+            var mailOptions = {
+              from: 'gamecowo12345@gmail.com',
+              to: user.email,
+              subject: 'Invoice',
+              text: 
+              `======================= INVOICE ==================
+               Name: ${user.getFullName()}
+
+               Amount: ${user.TransactionB2Bs[0].amount}
+
+               qtyPrice: ${user.TransactionB2Bs[0].qtyPrice}
+
+
+               ___________________________________________________+
+
+               SubTotal: ${user.TransactionB2Bs[0].subTotal}
+
+
+               ======================================================
+
+               please Transfer As fast as you can
+                Mandiri a/n :Trade Naw
+                No rek: 600014567995274
+
+                contact us : Trade-Naw@company.com
+                        `
+            };
+            
+            transporter.sendMail(mailOptions, function(error, info){
+              if (error) {
+                console.log(error)
+            } else {
+                console.log(`email send : ${info.response}`) 
+            }
+            });
+        res.redirect('/?info= success sent invoice')
+        })
+     
         .catch((err) => {
             res.redirect(`/?error= ${err}`)
         })
@@ -117,32 +192,7 @@ Routes.get("/delete", (req, res) => {
         })
 })
 
-Routes.post("/email/:userId", (req, res) => {
-    var nodemailer = require('nodemailer');
 
-var transporter = nodemailer.createTransport({
-  service: 'gmail',
-  auth: {
-    user: 'gamecowo12345@gmail.com',
-    pass: 'gamecowo54321'
-  }
-});
-
-var mailOptions = {
-  from: 'gamecowo12345@gmail.com',
-  to: 'myfriend@yahoo.com',
-  subject: 'Ivoice',
-  text: 'That was easy!'
-};
-
-transporter.sendMail(mailOptions, function(error, info){
-  if (error) {
-    console.log(error);
-  } else {
-    console.log('Email sent: ' + info.response);
-  }
-});
-})
 
 
 
